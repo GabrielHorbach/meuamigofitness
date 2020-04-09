5c6f0a67db0f91137d580b1df5c0e61f40c41638
GabrielHorbach <gabrielhorbachmachado@hotmail.com>
2020-04-09 15:34:02 -0300

    feat: implements new service and add api config

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,23 +1,28 @@
-import api from './api';
+import { api } from './api';
 
 const UserService = {
-  getUserData: async (id, token) => {
+  getUserData: async id => {
     let user = {};
     try {
-      const result = await api.get(`users/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const result = await api.get(`users/${id}`);
 
       if (result) {
         user = result.data;
       }
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
 
     return user;
+  },
+  updateUser: async (id, data) => {
+    try {
+      const result = await api.put(`users/${id}`, data);
+
+      return result.data;
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,30 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+
+async function getToken() {
+  const user = JSON.parse(await AsyncStorage.getItem('USER_DATA'));
+  return user.token;
+}
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
 
-export default api;
+const noAuthApi = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+
+api.interceptors.request.use(
+  async config => {
+    const token = await getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+export { api, noAuthApi };
